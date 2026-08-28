ef470f5c709c2d62c62dd1a022c73d098ac33d4f
danyalkhan0017 <89699249+danyalkhan0017@users.noreply.github.com>
2026-08-28 16:13:24 +0500

    Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+module.exports = nextConfig;
